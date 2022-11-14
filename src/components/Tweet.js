6548781fc9b6679d76a0d12ce6c2af59dae17d32
Lisa-Maria Bieker <lisa.bieker@web.de>
2022-11-14 15:57:05 +0100

    Iteration5

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,4 +1,6 @@
-import ProfileImage from "./ProfileImage"
+import ProfileImage from "./ProfileImage";
+import User from "./User";
+
 function Tweet(props) {
   return (
     <div className="tweet">
@@ -6,10 +8,8 @@ function Tweet(props) {
 
       <div className="body">
         <div className="top">
-          <span className="user">
-            <span className="name">{props.tweet.user.name}</span>
-            <span className="handle">{props.tweet.user.handle}</span>
-          </span>
+          
+          <User userData={props.tweet.user} />
 
           <span className="timestamp">{props.tweet.timestamp}</span>
         </div>
